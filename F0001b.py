219cd3cd1448beaa73c927c12362a3a9b5f19faa
dalmadybence <54987648+dalmadybence@users.noreply.github.com>
2020-10-15 11:55:24 +0200

    Add files via upload

---
+++ F0001b.py
@@ -0,0 +1,6 @@
+print('Rásír a sok kis malac is!')
+print('"Én is, én is, én is, én is!"')
+
+#Ki fogja írni a szövegeket a shellben,
+#A másodikban pedig szintén kiírjuk a cuccokat,
+#és az " " jeleket.
